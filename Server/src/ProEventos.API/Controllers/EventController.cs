7a436ad9ce4868312f78f0d4df506354989b8444
Dienisson Almeida <dienisson.chinelatto@ndd.com.br>
2021-03-07 09:43:44 -0300

    feat: Implementação EF Core e Migrations

---
+++ Server/src/ProEventos.API/Controllers/EventController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public EventController(DataContext context)
+        {
+            this._context = context;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Event> Get()
+        {
+            return _context.Events.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Event Get(int id)
+        {
+            return _context.Events.Find(id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Example Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Post(int id)
+        {
+            return $"Example Put {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Example Delete {id}";
+        }
+    }
+}
---
+++ Server/src/ProEventos.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+
+        public DataContext()
+        {
+            
+        }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
